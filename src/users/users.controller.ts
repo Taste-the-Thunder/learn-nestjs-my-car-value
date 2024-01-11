a0c238e3b0e8e17e65b70b9d1a13aa7752e41560
Dhruvin Prajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2024-01-11 14:51:37 +0530

    CRUD perform on USER

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { CreateUserDto } from './dtos/create0user.dto';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { CreateUserDto } from './dtos/createuser.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/updateUser.dto';
 
 @Controller('users')
 export class UsersController {
@@ -10,5 +11,25 @@ export class UsersController {
   @Post('/signup')
   createUser(@Body() body: CreateUserDto){
     this.usersService.create(body.email, body.password)
+  }
+
+  @Get('/:id')
+  findUser(@Param('id') id: string){
+    return this.usersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string){
+    return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string){
+    return this.usersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDto){
+    return this.usersService.update(parseInt(id), body);
   }
 }
