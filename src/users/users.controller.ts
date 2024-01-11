46e74c2f067ae4b47769a0c5ba99d3257dcfb5a3
Dhruvin Prajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2024-01-11 10:57:44 +0530

    create user DTO

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create0user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto){
+    console.log(body);
+  }
+
+}
