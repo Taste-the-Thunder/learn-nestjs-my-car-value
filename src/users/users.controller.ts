5993eefb2f159f22b15470b73ab17c7e96ba256d
Dhruvin Prajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2024-01-12 12:03:05 +0530

    setup notFoundException

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createuser.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/updateUser.dto';
@@ -14,8 +14,13 @@ export class UsersController {
   }
 
   @Get('/:id')
-  findUser(@Param('id') id: string){
-    return this.usersService.findOne(parseInt(id));
+  async findUser(@Param('id') id: string){
+    const user = await this.usersService.findOne(parseInt(id)); 
+    if(!user){
+      throw new NotFoundException('user not found');
+    }
+    
+    return user;
   }
 
   @Get()
