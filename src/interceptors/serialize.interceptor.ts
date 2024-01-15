668b03579ca09cd8258c4275b0404237e1376080
Dhruvin Prajapati <35141691+dhruvinprajapati@users.noreply.github.com>
2024-01-15 13:32:17 +0530

    setup interseptor for any DTO

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -1,13 +1,14 @@
-import { UseInterceptors, NestInterceptor, CallHandler, ExecutionContext } from "@nestjs/common";
+import { NestInterceptor, CallHandler, ExecutionContext } from "@nestjs/common";
 import { plainToClass } from "class-transformer";
 import { Observable, map } from "rxjs";
 import { UserDto } from "src/users/dtos/user.dto";
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any){}
   intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
       return next.handle().pipe(
         map((data: any) => {
-          return plainToClass(UserDto, data, {
+          return plainToClass(this.dto, data, {
             excludeExtraneousValues: true
           });
         })
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dtos/createuser.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/updateUser.dto';
 import { SerializeInterceptor } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -14,7 +15,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password)
   }
 
-  @UseInterceptors(SerializeInterceptor)
+  @UseInterceptors(new SerializeInterceptor(UserDto))
   @Get('/:id')
   async findUser(@Param('id') id: string){
     console.log('handler is running');
